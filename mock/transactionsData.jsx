1e04a6595b52261011f2de82c4a4f1222d9e0779
FaisalMohammedElorm <mohammedelormf@gmail.com>
2024-06-23 18:51:05 +0100

    Transactions

---
+++ mock/transactionsData.jsx
@@ -0,0 +1,30 @@
+export const transactionsData = [
+  {
+    id:0,
+    icon: require("../assets/apple.png"),
+    title:"Apple Store",
+    subTitle:"Entertainment",
+    amount:"-$5,99",
+  },
+  {
+    id:1,
+    icon: require("../assets/spotify.png"),
+    title:"Apple Store",
+    subTitle:"Entertainment",
+    amount:"-$5,99",
+  },
+  {
+    id:2,
+    icon: require("../assets/moneyTransfer.png"),
+    title:"Apple Store",
+    subTitle:"Entertainment",
+    amount:"-$5,99",
+  },
+  {
+    id:3,
+    icon: require("../assets/grocery.png"),
+    title:"Apple Store",
+    subTitle:"Entertainment",
+    amount:"-$5,99",
+  },
+]
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -1,30 +1,171 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity , FlatList} from 'react-native'
 import React from 'react'
+import colors from "../config/colors";
+import { FontAwesome } from '@expo/vector-icons';
+import { arrowData } from '../mock/arrowData';
+import { transactionsData } from '../mock/transactionsData';
 
 const WelcomeScreen = () => {
   return (
     <View style={styles.wholeScreen}>
       <View style={styles.headerContainer}>
-        <Image source={require("../assets/profile.png")}/>
-        <Text>Welcome Back,</Text>
-        <Text>Eric Atsu</Text>
+        <View style={styles.imageAndText}>
+          <Image source={require("../assets/profile.png")} style={styles.profilePic}/>
+            <View style={styles.bothTexts}>
+                <Text style={{color:colors.lightgrey, fontSize:25, fontWeight:"600"}}>Welcome Back,</Text>
+                <View style={styles.textAndIcon}>
+                    <Text style={{fontSize:30, fontWeight:"700"}}>Eric Atsu</Text>
+                    <TouchableOpacity>
+                      <FontAwesome name="search" size={30} color="black" style={styles.searchIcon}/>
+                    </TouchableOpacity>
+                </View>
+                
+            </View>
+         
+        </View>
+        <View style={styles.cardContainer}>
+          <Image source={require("../assets/Card.png")} />
+        </View>
+        <View>
+            <FlatList
+              data={arrowData}
+              renderItem={({item}) => (
+                <View style={styles.bothImageAndTitleContainer}>
+
+
+                  <View>
+                  <TouchableOpacity>
+                        <View style={styles.imagesContainer}>
+                            <Image source={item.image} style={styles.iconImage}/>
+                        </View>
+                      </TouchableOpacity>
+                      <View style={styles.titleTextContainer}>
+                        <Text style={styles.titleText}>{item.title}</Text>
+                      </View>
+                  </View> 
+                          
+                            
+                
+                       
+                
+              
+              
+                </View>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              horizontal
+              showsHorizontalScrollIndicator = {false}
+            />
+        </View>
+        <View style={{flexDirection:"row", justifyContent:"space-between", margin:5}}>
+          <Text style={{fontSize:28, fontWeight:"700"}}>Transaction</Text>
+          <Text style={{color:colors.blue, fontWeight:"600", fontSize:20}}>See All</Text>
       </View>
-    </View>
-  )
-}
-const styles = StyleSheet.create({
-  wholeScreen:{
-    flex:1,
-    
-    backgroundColor:"#fff"
-  
-  },
-  headerContainer:{
-    flex:1,
-    marginTop:30,
-    justifyContent:"flex-start",
-    marginLeft:25
+      <View>
+        <FlatList
+          data={transactionsData}
+          renderItem={({item}) => (
+            <View style={styles.transactionContainer}>
+                <View style={{flexDirection:"row", justifyContent:"space-between"}}>
+                  <Image source={item.icon} style={styles.transactionIcon}/>
+                  <View>
+                    <Text>{item.title}</Text>
+                    <Text>{item.subTitle}</Text>
+                    <Text>{item.amount}</Text>
+                  </View>
+                </View>
+
+
+
+
+
+            </View>
+
+         
+          
+          )}
+        />
+      </View>
+
+      </View>
+      
+      </View>
+    )
   }
-});
-export default WelcomeScreen
+  const styles = StyleSheet.create({
+    wholeScreen:{
+      flex:1,
+      backgroundColor:"#fff"
+         
+          
+          
+          
+    },
+    headerContainer:{
+      flex:1,
+      marginTop:45,
+      justifyContent:"flex-start",
+      marginLeft:15
+    },
+    profilePic:{
+      height:70,
+      width:70,
+    },
+    imageAndText:{
+      flexDirection:"row",
+    },
+    bothTexts:{
+      marginLeft:20,
+      justifyContent:"center",
+      flexDirection:"column"
+    },
+    textAndIcon:{
+      justifyContent:"space-between",
+      flexDirection:"row",
+    },
+    searchIcon:{
+      marginLeft:150
+    },
+    cardContainer:{
+      justifyContent:"center",
+      alignItems:"center",
+      marginTop:25
+    },
+   bothImageAndTitleContainer:{
+    justifyContent:"center",
+    margin:12,
+    
+    
+    
+    
+   }, 
+   imagesContainer:{
+    alignSelf:"center",
+    backgroundColor: colors.lightgrey,
+    padding:25, 
+    borderRadius:50
+   },
+   titleTextContainer:{
+    alignItems:"center",
+    justifyContent:"center",
+    margin:12
+   },
+   titleText:{
+    fontSize:20,
+    fontWeight:"600"
+   },
+   transactionIcon:{
+    height:35,
+    width:45,
+   }
+
+    
+  });
+  export default WelcomeScreen
+      
+      
+          
+    
+    
+  
       
